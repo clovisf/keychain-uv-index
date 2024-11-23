b6ea9d146b6c2d6b6f6327de15ad5074bb9c7f8a
Clovis Fritzen <clovisf@gmail.com>
2024-11-23 09:33:52 -0300

    initial code commit

---
+++ uv-index-attiny85/uv-index-attiny85.ino
@@ -0,0 +1,13 @@
+#define guvas12d A3
+#define LED 0
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED, OUTUPT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
